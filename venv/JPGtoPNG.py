68346c22c08cda545dfac00241fce8a226f31641
zims <zims@inbox.lv>
2020-04-14 21:00:47 +0300

    jpg converter works/ need vid con

--- venv/JPGtoPNG.py
+++ venv/JPGtoPNG.py
@@ -5,11 +5,10 @@ import glob
 
 source_folder = input('Type the source folder of the files (a folder name in your main directory): ')
 output_folder = input('Folder name to output the .png files (one word): ')
+
 path_now = os.getcwd()
 path_source = f"{path_now}/{source_folder}"
 path = path_now + "/" + output_folder
-
-
 
 try:
     os.mkdir(path)
@@ -21,5 +20,4 @@ for filename in glob.glob(f'{source_folder}/*.jpg'):  # assuming jpg
     new_name = os.path.splitext(filename)[0]
     no_prefix = new_name.split('/')[1]
     im.save(f'{output_folder}/{no_prefix}.png')
-    # except
-
+print('All done!')
